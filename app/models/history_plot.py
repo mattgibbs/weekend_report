cb3ec84a4901d6e4ba054133dffbb619d0d2a60c
Matt Gibbs <mgibbs@gatech.edu>
2015-03-05 00:58:48 -0800

    Add convenience methods for getting archiver data for the plot.

--- app/models/history_plot.py
+++ app/models/history_plot.py
@@ -1,8 +1,18 @@
 from app import db
+import requests
 
 class HistoryPlot(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	report_id = db.Column(db.Integer, db.ForeignKey('report.id'))
-	pv = db.Column(db.String(140), nullable = False)
-	events = db.relationship('HistoryEvent', backref='history_plot', lazy='dynamic')
-	archiver_json = db.Column(db.Text())
+  id = db.Column(db.Integer, primary_key = True)
+  report_id = db.Column(db.Integer, db.ForeignKey('report.id'))
+  pv = db.Column(db.String(140), nullable = False)
+  events = db.relationship('HistoryEvent', backref='history_plot', lazy='dynamic')
+  archiver_json = db.Column(db.Text())
+  
+  def archiver_url(self):
+    base_url_string = "http://lcls-archapp.slac.stanford.edu/retrieval/data/getData.json?pv=mean_360({0})&from={1}&to={2}"
+    return base_url_string.format(self.pv, self.report.start.strftime('%Y-%m-%dT%H:%M:%S.000Z'), self.report.end.strftime('%Y-%m-%dT%H:%M:%S.000Z'))
+    
+  def get_archiver_data(self):
+    r = requests.get(self.archiver_url())
+    if r.status_code == 200 and r.headers['content-type'] == 'application/json':
+      self.archiver_json = r.text
