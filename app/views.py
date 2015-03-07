8f42d2cb5721be73eaa8aebfe9b73713558eba6b
Matt Gibbs <mgibbs@gatech.edu>
2015-03-06 22:05:02 -0800

    Use the 'display_order' of plots and programs to display them in a sorted order on the report.

--- app/views.py
+++ app/views.py
@@ -18,7 +18,12 @@ def show_report(reportid = None):
   report = db.session.query(models.Report).get(reportid)
   if report == None:
     abort(404)
-  return render_template("show_report.html", report=report)
+  report_items = []
+  report_items.extend(report.programs.all())
+  report_items.extend(report.history_plots.all())
+  report_items.sort(key=lambda report_item: report_item.display_order)
+  
+  return render_template("show_report.html", report=report, report_items=report_items)
   
 @app.route('/reports/new/')
 def new_report():
