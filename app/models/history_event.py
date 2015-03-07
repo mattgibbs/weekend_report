971d590ce711aa6a6923a247b4c7861b03007792
Matt Gibbs <mgibbs@gatech.edu>
2015-03-07 02:09:14 -0800

    Add a convenience method to return a ISO date string with UTC time zone identifier.

--- app/models/history_event.py
+++ app/models/history_event.py
@@ -1,7 +1,10 @@
 from app import db
 
 class HistoryEvent(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	history_plot_id = db.Column(db.Integer, db.ForeignKey('history_plot.id'))
-	timestamp = db.Column(db.DateTime(), nullable = False)
-	text = db.Column(db.String(60), nullable = False)
+  id = db.Column(db.Integer, primary_key = True)
+  history_plot_id = db.Column(db.Integer, db.ForeignKey('history_plot.id'))
+  timestamp = db.Column(db.DateTime(), nullable = False)
+  text = db.Column(db.String(60), nullable = False)
+  
+  def utc_iso_string(self):
+    return self.timestamp.isoformat() + '.000Z'
