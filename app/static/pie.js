6af50e9b3a9918a9c003247de76dbb60c3b8c35d
Matt Gibbs <mgibbs@gatech.edu>
2015-03-06 23:39:33 -0800

    Keep labels from overlapping.

--- app/static/pie.js
+++ app/static/pie.js
@@ -1,6 +1,7 @@
 function makePie(element, dataset, w, h) {
   var outerRadius = w / 2;
   var innerRadius = w / 3;
+  var labelRadius = (outerRadius + innerRadius) / 2;
   var arc = d3.svg.arc()
   				.innerRadius(innerRadius)
   				.outerRadius(outerRadius);
@@ -43,15 +44,63 @@ function makePie(element, dataset, w, h) {
   	  .attr("class", "label")
   	  .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
 
-  labelgroups.append("text")
+  var textLabels = labelgroups.append("text")
             .attr("transform", function(d) {
               return "translate(" + arc.centroid(d) + ")";
             })
             .attr("dx", "0.5em")
-            .attr("text-anchor", "middle")
+            .attr("text-anchor", function (d, i) {
+              centroid = arc.centroid(d);
+              midAngle = Math.atan2(centroid[1], centroid[0]);
+              x = Math.cos(midAngle) * labelRadius;
+              return (x > 0) ? "start" : "end";
+            })
             .text(function(d) {
-              return d.data.value + " hours";
+              return d.data.value + " hrs";
             });
+  //Space the labels out.  Fancy!
+  alpha = 3;
+  spacing = 8;
+
+  function relax() {
+      again = false;
+      textLabels.each(function (d, i) {
+          a = this;
+          da = d3.select(a);
+          y1 = da.attr("y");
+          textLabels.each(function (d, j) {
+              b = this;
+              // a & b are the same element and don't collide.
+              if (a == b) return;
+              db = d3.select(b);
+              // a & b are on opposite sides of the chart and
+              // don't collide
+              if (da.attr("text-anchor") != db.attr("text-anchor")) return;
+              // Now let's calculate the distance between
+              // these elements. 
+              y2 = db.attr("y");
+              deltaY = y1 - y2;
+              // Our spacing is greater than our specified spacing,
+              // so they don't collide.
+              if (Math.abs(deltaY) > spacing) return;
+  
+              // If the labels collide, we'll push each 
+              // of the two labels up and down a little bit.
+              again = true;
+              sign = deltaY > 0 ? 1 : -1;
+              adjust = sign * alpha;
+              da.attr("y",+y1 + adjust + 3);
+              db.attr("y",+y2 - adjust + 3);
+          });
+      });
+      
+      if(again) {
+        setTimeout(relax,20)
+      }
+  }
+
+  relax();
+            
             
   var legendRectSize = 12;
   var legendSpacing = 4;
