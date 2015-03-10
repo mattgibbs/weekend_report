8b7d0b81f68176a497c10347ddec650c8805d0c5
Matt Gibbs <mgibbs@gatech.edu>
2015-03-09 18:01:00 -0700

    Refactoring: move code to instantiate a report from a POST form into the model.

--- app/models/report.py
+++ app/models/report.py
@@ -1,8 +1,75 @@
-from app import db
+from datetime import datetime
+from dateutil import parser
+
+from app import db, timeconverter
+from program import Program
+from note import Note
+from history_plot import HistoryPlot
+from history_event import HistoryEvent
+from downtime_data import DowntimeData
 
 class Report(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	start = db.Column(db.DateTime(), nullable = False)
-	end = db.Column(db.DateTime(), nullable = False)
-	programs = db.relationship('Program', backref='report', lazy='dynamic')
-	history_plots = db.relationship('HistoryPlot', backref='report', lazy='dynamic')
+  id = db.Column(db.Integer, primary_key = True)
+  start = db.Column(db.DateTime(), nullable = False)
+  end = db.Column(db.DateTime(), nullable = False)
+  programs = db.relationship('Program', backref='report', lazy='dynamic')
+  history_plots = db.relationship('HistoryPlot', backref='report', lazy='dynamic')
+  
+  def from_form(self, form):
+    #Parse the start time string and convert to UTC before storing it.
+    local_start = parser.parse(form['start'], ignoretz = True)
+    local_start.replace(hour=8)
+    self.start = timeconverter.convert_to_UTC(local_start)
+  
+    #Parse the end time string and convert to UTC before storing it.
+    local_end = parser.parse(form['end'], ignoretz = True)
+    local_end.replace(hour=8)
+    self.end = timeconverter.convert_to_UTC(local_end)
+  
+    programs = filter(None, form.getlist('program'))
+    for program_number in programs:
+      new_program = Program()
+      new_program.report = self
+      new_program.name = form['name-' + program_number]
+      new_program.time_note = form['time_note-' + program_number]
+      new_program.config_note = form['config_note-' + program_number]
+      new_program.performance_note = form['performance_note-' + program_number]
+      new_program.display_order = int(form['program-order-' + program_number])
+      other_notes = filter(None, form.getlist('other_note-' + program_number))
+      for other_note in other_notes:
+        new_note = Note()
+        new_note.program = new_program
+        new_note.text = other_note
+      new_downtime = DowntimeData()
+      new_downtime.program = new_program
+      if form['downtime-' + program_number]:
+        new_downtime.downtime = float(form['downtime-' + program_number])
+      else:
+        new_downtime.downtime = 0
+      if form['config_changes-' + program_number]:
+        new_downtime.config_changes = float(form['config_changes-' + program_number])
+      else:
+        new_downtime.config_changes = 0
+      if form['delivered-' + program_number]:
+        new_downtime.delivered = float(form['delivered-' + program_number])
+      else:
+        new_downtime.delivered = 0
+      
+    history_plots = filter(None, form.getlist('history_plot'))
+    for plot_number in history_plots:
+      new_plot = HistoryPlot()
+      new_plot.report = self
+      new_plot.pv = form['pv-' + plot_number]
+      new_plot.display_order = int(form['plot-order-' + plot_number])
+      events = form.getlist("plot[{0}]event".format(plot_number))
+      for event_number in events:
+        new_event = HistoryEvent()
+        new_event.text = form["plot[{0}]event[{1}]-text".format(plot_number, event_number)]
+        if not new_event.text:
+          continue
+        local_timestring = form["plot[{0}]event[{1}]-timestamp".format(plot_number, event_number)]
+        if not local_timestring:
+          continue
+        new_event.history_plot = new_plot
+        local_timestamp = parser.parse(local_timestring, ignoretz = True)
+        new_event.timestamp = timeconverter.convert_to_UTC(local_timestamp)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app, db, models, timeconverter
-from datetime import datetime
-from dateutil import parser
+from app import app, db, models
+
 
 @app.route('/')
 def root_index():
@@ -31,79 +30,18 @@ def new_report():
   
 @app.route('/create/', methods=['POST'])
 def create_report():
-  new_report = models.Report()
+  
   print(request.form)
+  report = models.Report()
+  db.session.add(report)
   try:
-    db.session.add(new_report)
-    #Parse the start time string and convert to UTC before storing it.
-    local_start = parser.parse(request.form['start'], ignoretz = True)
-    local_start.replace(hour=8)
-    new_report.start = timeconverter.convert_to_UTC(local_start)
-  
-    #Parse the end time string and convert to UTC before storing it.
-    local_end = parser.parse(request.form['end'], ignoretz = True)
-    local_end.replace(hour=8)
-    new_report.end = timeconverter.convert_to_UTC(local_end)
-  
-    programs = filter(None, request.form.getlist('program'))
-    for program_number in programs:
-      new_program = models.Program()
-      db.session.add(new_program)
-      new_program.report = new_report
-      new_program.name = request.form['name-' + program_number]
-      new_program.time_note = request.form['time_note-' + program_number]
-      new_program.config_note = request.form['config_note-' + program_number]
-      new_program.performance_note = request.form['performance_note-' + program_number]
-      new_program.display_order = int(request.form['program-order-' + program_number])
-      other_notes = filter(None, request.form.getlist('other_note-' + program_number))
-      for other_note in other_notes:
-        new_note = models.Note()
-        db.session.add(new_note)
-        new_note.program = new_program
-        new_note.text = other_note
-      new_downtime = models.DowntimeData()
-      db.session.add(new_downtime)
-      new_downtime.program = new_program
-      if request.form['downtime-' + program_number]:
-        new_downtime.downtime = float(request.form['downtime-' + program_number])
-      else:
-        new_downtime.downtime = 0
-      if request.form['config_changes-' + program_number]:
-        new_downtime.config_changes = float(request.form['config_changes-' + program_number])
-      else:
-        new_downtime.config_changes = 0
-      if request.form['delivered-' + program_number]:
-        new_downtime.delivered = float(request.form['delivered-' + program_number])
-      else:
-        new_downtime.delivered = 0
-    
-    history_plots = filter(None, request.form.getlist('history_plot'))
-    for plot_number in history_plots:
-      new_plot = models.HistoryPlot()
-      db.session.add(new_plot)
-      new_plot.report = new_report
-      new_plot.pv = request.form['pv-' + plot_number]
-      new_plot.display_order = int(request.form['plot-order-' + plot_number])
-      events = request.form.getlist("plot[{0}]event".format(plot_number))
-      for event_number in events:
-        new_event = models.HistoryEvent()
-        new_event.text = request.form["plot[{0}]event[{1}]-text".format(plot_number, event_number)]
-        if not new_event.text:
-          continue
-        local_timestring = request.form["plot[{0}]event[{1}]-timestamp".format(plot_number, event_number)]
-        if not local_timestring:
-          continue
-        db.session.add(new_event)
-        new_event.history_plot = new_plot
-        local_timestamp = parser.parse(local_timestring, ignoretz = True)
-        new_event.timestamp = timeconverter.convert_to_UTC(local_timestamp)
-        
+    report.from_form(request.form)
     db.session.commit()
   except:
     db.session.rollback()
     raise
   
-  flash('New report saved. <a href="' + url_for('show_report', reportid = new_report.id) + '">Click here to view it.</a>')
+  flash('New report saved. <a href="' + url_for('show_report', reportid = report.id) + '">Click here to view it.</a>')
   return redirect(url_for('report_index'))
     
   
