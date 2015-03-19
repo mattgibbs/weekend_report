507609424dbba3ece1c0d9c96fb8728bdc47ed90
Matt Gibbs <mgibbs@gatech.edu>
2015-03-19 07:44:42 -0700

    Add the ability to convert from UTC to the local timezone.

--- app/timeconverter.py
+++ app/timeconverter.py
@@ -7,3 +7,8 @@ def convert_to_UTC(naive_datetime):
   local_datetime = local.localize(naive_datetime, is_dst=None)
   utc_datetime = local_datetime.astimezone(pytz.utc)
   return utc_datetime.replace(tzinfo=None)
+  
+def convert_to_local(naive_datetime):
+  utc_datetime = naive_datetime.replace(tzinfo=pytz.utc)
+  local_datetime = utc_datetime.astimezone(local)
+  return local_datetime.replace(tzinfo=None)
