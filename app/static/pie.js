3d9112eff65fd7da507675ad2a73c3d5f163f5a2
Matt Gibbs <mgibbs@gatech.edu>
2016-08-06 15:43:05 -0700

    WIP: New pie chart with fancier labels.

--- app/static/pie.js
+++ app/static/pie.js
@@ -1,16 +1,20 @@
 function makePie(element, dataset, w, h) {
   var margin = 30;
-  var outerRadius = (w) / 2;
-  var innerRadius = (w) / 3;
-  var labelRadius = (outerRadius + innerRadius) / 2;
+  var radius = Math.min(w, h) / 2;
   var arc = d3.svg.arc()
-  				.innerRadius(innerRadius)
-  				.outerRadius(outerRadius);
-
+  				.innerRadius(radius * 0.8)
+  				.outerRadius(radius * 0.4);
+  var outerArc = d3.swg.arc()
+          .innerRadius(radius * 0.9)
+          .outerRadius(radius * 0.9);
+          
   var pie = d3.layout.pie()
+              .sort(null)
               .value(function(d) { return d.value; })
               .padAngle(0.5*Math.PI/180);
-
+  
+  
+  
   //Easy colors accessible via a 10-step ordinal scale
   //var color = d3.scale.category10();
   var color = d3.scale.ordinal()
@@ -19,113 +23,117 @@ function makePie(element, dataset, w, h) {
   //Create SVG element
   var svg = d3.select(element)
   			.append("svg")
-  			.attr("width", w)
-  			.attr("height", h);
-
-  //Set up groups
-  var arcs = svg.selectAll("g.arc")
-  			  .data(pie(dataset))
-  			  .enter()
-  			  .append("g")
-  			  .attr("class", "arc")
-  			  .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-
-  //Draw arc paths
-  arcs.append("path")
-      .attr("fill", function(d, i) {
-      	return color(d.data.title);
-      })
-      .attr("d", arc);
-
-  //Labels
-  var labelgroups = svg.selectAll("g.label")
-  	  .data(pie(dataset))
-  	  .enter()
-  	  .append("g")
-  	  .attr("class", "label")
-  	  .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-
-  var textLabels = labelgroups.append("text")
-            .attr("transform", function(d) {
-              return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("dx", "0.5em")
-            .attr("text-anchor", function (d, i) {
-              centroid = arc.centroid(d);
-              midAngle = Math.atan2(centroid[1], centroid[0]);
-              x = Math.cos(midAngle) * labelRadius;
-              return (x > 0) ? "start" : "middle";
-            })
-            .text(function(d) {
-              return d.data.value + " hrs";
-            });
-  //Space the labels out.  Fancy!  Courtesy of http://jsfiddle.net/thudfactor/HdwTH/
-  alpha = 3;
-  spacing = 8;
-
-  function relax() {
-      again = false;
-      textLabels.each(function (d, i) {
-          a = this;
-          da = d3.select(a);
-          y1 = da.attr("y");
-          textLabels.each(function (d, j) {
-              b = this;
-              // a & b are the same element and don't collide.
-              if (a == b) return;
-              db = d3.select(b);
-              // a & b are on opposite sides of the chart and
-              // don't collide
-              if (da.attr("text-anchor") != db.attr("text-anchor")) return;
-              // Now let's calculate the distance between
-              // these elements. 
-              y2 = db.attr("y");
-              deltaY = y1 - y2;
-              // Our spacing is greater than our specified spacing,
-              // so they don't collide.
-              if (Math.abs(deltaY) > spacing) return;
+        .append("g");
+  svg.append("g").attr("class", "slices");
+  svg.append("g").attr("class", "labels");
+  svg.append("g").attr("class", "lines");
+  svg.attr("transform", "translate(" + w/2 +  "," + h/2 + ")");
+  var key = function(d){ return d.data.title; };
   
-              // If the labels collide, we'll push each 
-              // of the two labels up and down a little bit.
-              again = true;
-              sign = deltaY > 0 ? 1 : -1;
-              adjust = sign * alpha;
-              da.attr("y",+y1 + adjust + 3);
-              db.attr("y",+y2 - adjust + 3);
-          });
-      });
-      
-      if(again) {
-        setTimeout(relax,20)
-      }
+  function mergeWithFirstEqualZero(first, second) {
+    var secondSet = d3.set();
+    second.forEach(function(d) { secondSet.add(d.title); });
+    var onlyFirst = first
+      .filter(function(d){ return !secondSet.has(d.title) })
+      .map(function(d){ return {title: d.title, value: 0}; });
+    return d3.merge([second, onlyFirst])
+      .sort(function(a,b) { return d3.ascending(a.title, b.title); });
   }
-
-  relax();
-            
-            
-  var legendRectSize = 12;
-  var legendSpacing = 4;
-  var legend = svg.selectAll('.legend')
-                  .data(["Downtime", "Config Changes", "Delivered", "Tuning"])
-                  .enter()
-                  .append('g')
-                  .attr('class','legend')
-                  .attr('transform', function(d,i) {
-                    var height = legendRectSize + legendSpacing;
-                    var offset = height * 3 / 2;
-                    var horz = -4.5 * legendRectSize;
-                    var vert = i * height - offset;
-                    return 'translate(' + (horz + outerRadius) + ',' + (vert + outerRadius) + ')';
-                  });
   
-  legend.append('rect')
-        .attr('width', legendRectSize)
-        .attr('height', legendRectSize)
-        .style('fill', color)
-        .style('stroke', color);
-  
-  legend.append('text')
-        .attr('x', legendRectSize + legendSpacing)
-        .attr('y', legendRectSize)
-        .text(function(d) { return d; });
+  function change(data) {
+    var duration = 250;
+    var data0 = svg.select(".slices").selectAll("path.slice")
+      .data().map(function(d) { return d.data });
+    if (data0.length == 0) { data0 = data; }
+    var was = mergeWithFirstEqualZero(data, data0);
+    var is = mergeWithFirstEqualZero(data0, data);
+    var slice = svg.select(".slices").selectAll("path.slice").data(pie(was), key);
+    slice.enter()
+      .insert("path")
+      .attr("class", "slice")
+      .style("fill", function(d) { return color(d.data.title); })
+      .each(function(d) {
+        this._current = d;
+      });
+    slice = svg.select(".slices").selectAll("path.slice").data(pie(is), key);
+    slice.transition().duration(duration).attrTween("d", function(d) {
+      var interpolate = d3.interpolate(this._current, d);
+      var _this = this;
+      return function(t) {
+        _this._current = interpolate(t);
+        return arc(_this._current);
+      };
+    });
+    slice = svg.select(".slices").selectAll("path.slice").data(pie(data), key);
+    slice.exit().transition().delay(duration).duration(0).remove();
+    
+    var text = svg.select(".labels").selectAll("text").data(pie(was), key);
+    text.enter()
+      .append("text")
+      .attr("dy", "0.35em")
+      .style("opacity", 0)
+      .text(function(d) {
+        return d.data.title;
+      })
+      .each(function(d) {
+        this._current = d;
+      });
+    function midAngle(d) {
+      return d.startAngle + (d.endAngle - d.startAngle)/2;
+    }
+    text = svg.select(".labels").selectAll("text").data(pie(is), key);
+    text.transition()
+      .duration(duration)
+      .style("opacity", function(d) {
+        return d.data.value == 0 ? 0 : 1;
+      })
+      .attrTween("transform", function(d) {
+        var interpolate = d3.interpolate(this._current, d);
+        var _this = this;
+        return function(t) {
+          var d2 = interpolate(t);
+          _this._current = d2;
+          var pos = outerArc.centroid(d2);
+          pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+          return "translate(" + pos + ")";
+        };
+      })
+      .styleTween("text-anchor", function(d) {
+        var interpolate = d3.interpolate(this._current, d);
+        return function(t) {
+          var d2 = interpolate(t);
+          return midAngle(d2) < Math.PI ? "start":"end";
+        };
+      });
+      text = svg.select(".labels").selectAll("text").data(pie(data), key);
+      text.exit().transition().delay(duration).remove();
+      
+      var polyline = svg.select(".lines").selectAll("polyline").data(pie(was), key);
+      polyline.enter()
+        .append("polyline")
+        .style("opacity", 0)
+        .each(function(d) {
+          this._current = d;
+        });
+      polyline = svg.select(".lines").selectAll("polyline").data(pie(is), key);
+      polyline.transition()
+        .duration(duration)
+        .style("opacity", function(d) {
+          return d.data.value == 0 ? 0 : 0.5;
+        })
+        .attrTween("points", function(d) {
+          //this._current = this._current;
+          var interpolate = d3.interpolate(this._current, d);
+          var _this = this;
+          return function(t) {
+            var d2 = interpolate(t);
+            _this._current = d2;
+            var pos = outerArc.centroid(d2);
+            pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+            return [arc.centroid(d2), outerArc.centroid(d2), pos];
+          };
+        });
+      polyline = svg.select(".lines").selectAll("polyline").data(pie(data), key);
+      polyline.exit().transition().delay(duration).remove();
+  };
 }
