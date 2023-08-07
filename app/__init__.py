7da62dd2ca2330a7e6884284339571569596ee9c
Matt Gibbs <mgibbs@slac.stanford.edu>
2023-08-07 10:00:21 -0700

    Add proxy fix.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 try:
 	os.environ["WEEKENDREPORT_SETTINGS"]
 	app.config.from_envvar('WEEKENDREPORT_SETTINGS')
